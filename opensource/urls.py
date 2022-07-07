0e0c513419ae4f19bd5a0e0e6ccb8728e2536cf4
abdallh <abdallhadly6@gmail.com>
2022-07-07 16:22:22 +0200

    view and url handle commit_3

---
+++ opensource/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('student/<st_id>', views.getStudent),
+    path('all', views.getAllStudents),
+]
--- opensource/views.py
+++ opensource/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest, HttpResponse
+from . models import Student , Track
 # Create your views here.
+
+def getStudent (request , st_id):
+    st = Student.objects.get(id = st_id)
+    return HttpResponse("<h1>Student name : </h1>" + st.fname)
+
+def getAllStudents (request):
+    all_stedents = Student.objects.all()
+    context = {'students' : all_stedents}
+    return render(request , 'opensource/students.html' , context)
